319e2f67cf396b98cbc5287d903b66993dbb7104
Lamanover <buzek8a@icloud.com>
2024-12-28 00:02:34 +0300

    additional_task_1 28.12.24

---
+++ Test_3.py
@@ -0,0 +1,26 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(*args):
+    total_sum = 0
+    for i in args:
+        if isinstance(i, (int, float)):
+            total_sum += i
+        elif isinstance(i, str):
+            total_sum += len(i)
+        elif isinstance(i, (list, tuple, set)):
+            total_sum += calculate_structure_sum(*i)
+        elif isinstance(i, dict):
+            for key, value in i.items():
+                total_sum += len(key)
+                total_sum += calculate_structure_sum(value)
+    return total_sum
+
+result = calculate_structure_sum(data_structure)
+print(result)
