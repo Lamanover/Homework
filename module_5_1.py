576efc9fec56981b016e69b342bab87323982b74
Lamanover <buzek8a@icloud.com>
2025-01-10 23:05:13 +0300

    module_5_2 10.01.25

--- module_5_1.py
+++ module_5_1.py
@@ -3,6 +3,12 @@ class house:
         self.name = name
         self.number_of_flors = number_of_flors
 
+    def __str__(self):
+       return (f'Название: {self.name}, колличество этажей: {self.number_of_flors}')
+
+    def __len__(self):
+        return self.number_of_flors
+
     def go_to(self, new_floor):
         if self.number_of_flors >= new_floor:
             for i in range(1, new_floor + 1):
@@ -13,6 +19,10 @@ class house:
 
 
 h1 = house('ЖК Горский', 18)
-h2 = house('Домик в деревне', 2)
 h1.go_to(5)
+print(h1)
+print(len(h1))
+h2 = house('Домик в деревне', 2)
 h2.go_to(10)
+print(h2)
+print(len(h2))
